69ab46858b589f39903bc5754038d45e1ed9cb09
fray osorio <frayosorio@gmail.com>
2025-09-01 21:00:41 -0500

    Se crea la interfaz grafica de usuario

---
+++ src/Carta.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Carta {
+
+    private int indice;
+
+    public Carta(Random r) {
+        indice = r.nextInt(52) + 1;
+    }
+
+    public void mostrar(JPanel pnl, int x, int y) {
+        String archivoImagen = "imagenes/CARTA" + indice + ".jpg";
+        ImageIcon imgCarta=new ImageIcon(getClass().getResource(archivoImagen));
+        JLabel lblCarta=new JLabel();
+        lblCarta.setIcon(imgCarta);
+        lblCarta.setBounds(x, y, imgCarta.getIconWidth(), imgCarta.getIconHeight());
+        pnl.add(lblCarta);
+    }
+
+}
